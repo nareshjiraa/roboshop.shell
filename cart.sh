f5cfdc22472771ffa43824af958693a0b5bb3e5a
NARESH <nareshjiraa@gmail.com>
2023-04-23 14:17:39 +0530

    shipping

--- cart.sh
+++ cart.sh
@@ -1,9 +1,11 @@
+script_path=$(dirname 0$)
+source ${scipt_path}/common.sh
 echo -e "\e[33m<<<<<setup nodejs repo>>\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 echo -e "\e[33m<<<<<install nodejs >>\e[0m"
 yum install nodejs -y
 echo -e "\e[33m<<<<<add roboshop user >>\e[0m"
-useradd roboshop
+useradd ${app_user}
 echo -e "\e[33m<<<<<remove app directory >>\e[0m"
 rm -rf /app
 echo -e "\e[33m<<<<<create app directory >>\e[0m"
@@ -16,7 +18,7 @@ echo -e "\e[33m<<<<<download dependencies>>\e[0m"
 cd /app
 npm install
 echo -e "\e[33m<<<<<setup suystemd service>>\e[0m"
-cp /home/centos/roboshop.shell/cart.service /etc/systemd/system/cart.service
+cp ${scipt_path}/cart.service /etc/systemd/system/cart.service
 echo -e "\e[33m<<<<<load service>>\e[0m"
 systemctl daemon-reload
 echo -e "\e[33m<<<<<start service>>\e[0m"
