292bc6fb961ca46422ecb3e0917dca3ddd9deee4
NARESH <nareshjiraa@gmail.com>
2023-05-06 12:50:27 +0530

    shipping

--- catalogue.sh
+++ catalogue.sh
@@ -1,10 +1,12 @@
 script=$(realpath "0$")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+
 component=catalogue
 func_nodejs
+schema_setup=mongo
+
 echo -e "\e[35m>>>copy mongo repo file\e[0m"
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+
 echo -e "\e[33m>>>install mongod client\e[0m"
-yum install mongodb-org-shell -y
-mongo --host mongodb-dev.mahadevops.online </app/schema/catalogue.js
+
--- common.sh
+++ common.sh
@@ -1,28 +1,55 @@
 app_user=roboshop
+print_head() {
+  echo -e "\e[35m>>>>>>$1<<<<<<<\e[0m"
+}
+
+schema_setup(){
+  if [ "$schema_setup" == "mongo" ];then
+    print_head "copy MongoDB repo"
+    cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+
+    print_head "install MongoDb client"
+    yum install mongodb-org-shell -y
+
+    print_head "load schema"
+    mongo --host mongodb-dev.mahadevops.online </app/schema/catalogue.js
+    fi
+}
 
 func_nodejs() {
-  echo -e "\e[33m<<<<<setup nodejs repo>>\e[0m"
+  print_head "setup nodejs repo"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-  echo -e "\e[33m<<<<<install nodejs >>\e[0m"
+
+  print_head "install nodejs"
   yum install nodejs -y
-  echo -e "\e[33m<<<<<add roboshop user >>\e[0m"
+
+  print_head"add roboshop user"
   useradd ${app_user}
-  echo -e "\e[33m<<<<<remove app directory >>\e[0m"
+
+  print_head "remove app directory"
   rm -rf /app
-  echo -e "\e[33m<<<<<create app directory >>\e[0m"
+
+  print_head "create app directory"
   mkdir /app
-  echo -e "\e[33m<<<<<download app code >>\e[0m"
+
+  print_head "download app code"
   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
   cd /app
   unzip /tmp/${component}.zip
-  echo -e "\e[33m<<<<<download dependencies>>\e[0m"
+
+  print_head "download dependencies"
   cd /app
   npm install
-  echo -e "\e[33m<<<<<setup suystemd service>>\e[0m"
+
+  print_head "setup suystemd service"
   cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
-  echo -e "\e[33m<<<<<load service>>\e[0m"
+
+  print_head"load service"
   systemctl daemon-reload
-  echo -e "\e[33m<<<<<start service>>\e[0m"
+
+  print_head "start service"
   systemctl enable ${component}
   systemctl start ${component}
+
+  schema_setup
 }
